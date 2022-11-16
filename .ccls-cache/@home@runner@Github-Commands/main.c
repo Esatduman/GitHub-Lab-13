89d50383e1b09212b0582e896754562ff5e929c5
Esat Duman <102714064+Esatduman@users.noreply.github.com>
2022-11-16 18:34:15 +0000

    GitHub commands

--- .ccls-cache/@home@runner@Github-Commands/main.c
+++ .ccls-cache/@home@runner@Github-Commands/main.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+ void eduman2() {
+  printf("Esat Duman\n");
+  printf("We learned that git and GitHub is a really nice tool to use for project collaboration\n");
 }
